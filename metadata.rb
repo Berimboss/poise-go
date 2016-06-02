742069b49659a9272778a160014da130fc17be6f
LouTheBrew <louthebrew@gmail.com>
2016-06-01 22:42:07 -0700

    fixe

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'poise-go'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures poise-go'
+long_description 'Installs/Configures poise-go'
+version '0.1.0'
